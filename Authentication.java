61c631284e0f6a669caf39d601c7ed88a12fa3f7
rober <rober@DESKTOP-G35UCGI>
2019-05-16 12:51:22 -0400

    change

--- Authentication.java
+++ Authentication.java
@@ -16,23 +16,37 @@ import com.google.api.client.http.UrlEncodedContent;
 
 import com.google.appengine.api.utils.SystemProperty;
 
-public class Authentication {
-<<<<<<< HEAD
+public class Authentication extends LoginServlet {
+
 	
 	private static String client_id;
 	private static String code;
 	private static String state;
 	private static String redirect_url;
-	redirect_url = ;
+	
+	public Authentication() {
+		this.client_id = "";
+		this.code = "";
+		this.state = "";
+		this.redirected_url = ;
+	}
 	
 	private string getUrl() {
 		return Url;
 	}
 	
-	private string checkUrl(String Url) {
+	private void checkUrl(String Url) throws IOException {
 		if (redirect_url != Url) {
 			throws new IOException;
 		}
 	}
-
+	
+	private String stateChanges() {
+		int n = 20;
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvwxyz";
+		StringBuilder sb = new StringBuilder(n);
+		int index = (int)(AlphaNumericString.length()) * Math.random());
+		sb.append(AlphaNumericString.charAt(index));
+		this.code = sb.toString();
+		return this.code;
 }
